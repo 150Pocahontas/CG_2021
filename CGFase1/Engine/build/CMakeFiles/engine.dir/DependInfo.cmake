498875605102a70d581143b5ea5a572d454283ff
Jaime Abreu <61059657+Shrek-5@users.noreply.github.com>
2021-04-03 14:50:22 +0100

    fase 1 refresh

---
+++ CGFase1/Engine/build/CMakeFiles/engine.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pawinha/Desktop/CGFase1-master/CGFase1/Engine/engine.cpp" "/home/pawinha/Desktop/CGFase1-master/CGFase1/Engine/build/CMakeFiles/engine.dir/engine.cpp.o"
+  "/home/pawinha/Desktop/CGFase1-master/CGFase1/Engine/tinyxml2.cpp" "/home/pawinha/Desktop/CGFase1-master/CGFase1/Engine/build/CMakeFiles/engine.dir/tinyxml2.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
